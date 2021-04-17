101833f9caa5bd1cc006895cf7d732fdc130b45c
tanishq-agarwal <agarwaltanishq35@gmail.com>
2021-04-17 12:59:57 +0530

    print numbers from 1 to n

--- print increasing/Main.java
+++ print increasing/Main.java
@@ -11,11 +11,11 @@ public class Main {
     }
 
     public static void printDecreasing(int n){
-        if(n == 0){
+        if(n <= 0){
             return;
         }
-        System.out.println(n);
         printDecreasing(n-1);
+        System.out.println(n);
     }
 
 }
