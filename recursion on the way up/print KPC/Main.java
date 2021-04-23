03bf24fd5da5dfc92984ee95f68d16013e121707
tanishq-agarwal <agarwaltanishq35@gmail.com>
2021-04-23 12:36:00 +0530

    print KYC

---
+++ recursion on the way up/print KPC/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        printKPC(str, "");
+    }
+
+    static String codes[] = { ".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    public static void printKPC(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+
+        String codeforch = codes[ch - '0'];
+        for (int i = 0; i <= codeforch.length(); i++) {
+            char chcode = codeforch.charAt(i);
+            printKPC(ros, ans + chcode);
+        }
+    }
+
+}
